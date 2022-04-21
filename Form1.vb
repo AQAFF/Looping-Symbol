3af27aa0c25c05ba2af3c1524265d2a8f77b3b5f
TUF <TUF@LAPTOP-257ULQGP>
2022-04-22 01:14:40 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim y As Integer
+        For y = 1 To 5
+            TextBox1.Text = TextBox1.Text & "$" & vbCr
+
+        Next y
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
